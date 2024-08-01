27be3af8f5fb3b30e67099501bb1d897a80f0654
iSlxp <ashank.mehrotra@hotmail.com>
2024-08-01 12:32:57 -0700

    stayBlessed

--- main.py
+++ main.py
@@ -11,65 +11,76 @@ def app():
 
    phrase = "The quick brown fox jumped over the lazy dog."
 
+   def levDistance(s1, s2):
+    if len(s1) < len(s2):
+        return levDistance(s2, s1)
+
+    if len(s2) == 0:
+        return len(s1)
+
+    previous_row = range(len(s2) + 1)
+    for i, c1 in enumerate(s1):
+        current_row = [i + 1]
+        for j, c2 in enumerate(s2):
+            insertions = previous_row[j + 1] + 1
+            deletions = current_row[j] + 1
+            substitutions = previous_row[j] + (c1 != c2)
+            current_row.append(min(insertions, deletions, substitutions))
+        previous_row = current_row
+
+    return previous_row[-1]
+
+
    def startTimer(event):
       global startTime
       startTime = t.time()
       startButton.configure(state = "disabled")
 
    def endTimer(event):
-      global endTime
-      endTime = t.time()
+    global endTime
+    endTime = t.time()
 
-      # enable startButton 
-      startButton.configure(state = "enabled")
-      
-      # calculate final time
-      totalTime = endTime - startTime
+    # enable startButton 
+    startButton.configure(state="normal")
+    
+    # calculate final time
+    totalTime = endTime - startTime
 
-      # calculate WPM
+    # calculate WPM
+    typingSpeed = 9 / (totalTime / 60)
+    wordsPer = int(typingSpeed)
 
-      typingSpeed = 9 / (totalTime / 60)
-      
-      wordsPer = int(typingSpeed)
+    # Get the input text
+    inputText = typeText.get()
 
-      # accuracy check
+    # Calculate Levenshtein Distance
+    distance = levDistance(phrase, inputText)
+    accuracyInt = 100 * (1 - distance / max(len(phrase), len(inputText)))
+    accuracyPercentage = str(int(accuracyInt)) + "%"
 
-      phraseList = list(phrase)
-      inputList = list(typeText.get())
-      
-      accuracyCount = 0
-      
-      for i in range(len(phraseList)):
-         if i < len(inputList) and phraseList[i] == inputList[i]:
-            accuracyCount += 1
-         
-      # finalCalculation
+    topLevel = ctk.CTkToplevel(root)
+    topLevel.geometry("200x200+850+250")
+    topLevel.wm_title("Results")
+    topLevel.resizable(False, False)
+    topLevel.attributes('-topmost', True) 
 
-      global accuracy
-      accuracyPercentage = str(int((accuracyCount / len(phraseList)) * 100)) + "%"
+    # resultLabel
+    wpmLabel = ctk.CTkLabel(topLevel, text="wpm", font=("Lilita One", 16))
+    wpmLabel.pack()
 
-      topLevel = ctk.CTkToplevel(root)
-      topLevel.geometry("200x200+850+250")
-      topLevel.wm_title("results")
-      topLevel.resizable(False, False)
-      topLevel.attributes('-topmost', True) 
-      # resultLabel
-      wpmLabel = ctk.CTkLabel(topLevel, text = "wpm", font = ("Lilita One", 18))
-      wpmLabel.pack()
+    wpmResult = ctk.CTkLabel(topLevel, text=str(wordsPer), font=("Lilita One", 32))
+    wpmResult.pack()
 
-      wpmResult = ctk.CTkLabel(topLevel, text = wordsPer, font = ("Lilita One", 30))
-      wpmResult.pack()
+    accLabel = ctk.CTkLabel(topLevel, text="acc", font=("Lilita One", 16))
+    accLabel.pack()
 
-      accLabel = ctk.CTkLabel(topLevel, text = "acc", font = ("Lilita One", 18))
-      accLabel.pack()
+    accResult = ctk.CTkLabel(topLevel, text=accuracyPercentage, font=("Lilita One", 32))
+    accResult.pack()
 
-      accResult = ctk.CTkLabel(topLevel, text = accuracyPercentage, font = ("Lilita One", 30))
-      accResult.pack()
+    # closeButton
+    closeButton = ctk.CTkButton(topLevel, text="Close", hover=True, corner_radius=15, fg_color="#b80b0b", hover_color="#e82323", command=topLevel.destroy, font=("Lilita One", 18))
+    closeButton.place(x=100, y=175, anchor='s')
 
-      # closeButton
-      
-      closeButton = ctk.CTkButton(topLevel, text = "Close", hover = True, corner_radius=15, fg_color="#b80b0b", hover_color="#e82323", command = topLevel.destroy, font = ("Lilita One", 18))
-      closeButton.place(x = 100, y = 175, anchor = 's')
 
    def clearText():
       typeText.delete(0, ctk.END)
